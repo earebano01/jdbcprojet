04f3dc8d7c9c825a21bed0e1338a20621983b072
Adonis Rebano <earebano01@monccnb.ca>
2023-10-24 11:25:01 -0300

    commit initial pour projet 1 (v1) - amélioration

---
+++ src/main/java/com/jdbcprojet/jdbcprojet/JdbcprojetApplication.java
@@ -0,0 +1,13 @@
+package com.jdbcprojet.jdbcprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcprojetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdbcprojet/jdbcprojet/JdbcprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbcprojet.jdbcprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcprojetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
