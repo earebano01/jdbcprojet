1ebb0982c6b76df7efe8ded413c2e03c88fbc3cc
Adonis Rebano <earebano01@monccnb.ca>
2023-10-25 19:03:10 -0300

    modifié le controller pour chartjs

--- src/main/java/com/jdbcprojet/jdbcprojet/controller/TemperatureViewController.java
+++ src/main/java/com/jdbcprojet/jdbcprojet/controller/TemperatureViewController.java
@@ -1,9 +1,12 @@
 package com.jdbcprojet.jdbcprojet.controller;
 
+import com.jdbcprojet.jdbcprojet.model.Temperature;
 import com.jdbcprojet.jdbcprojet.repository.TemperatureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TemperatureViewController {
@@ -15,8 +18,20 @@ public class TemperatureViewController {
     }
 
     @GetMapping("/temperatureView")
-    public String getProductView(Model model) {
+    public String getTemperatureView(Model model) {
+        List<Temperature> temperatureList = temperatureRepository.findAll();
+        List<String> xValues = new ArrayList<>();
+        List<Float> yValues = new ArrayList<>();
+
+        for (Temperature temperature : temperatureList) {
+            xValues.add(temperature.getFormattedDate());
+            yValues.add(temperature.getTemperature());
+        }
+
         model.addAttribute("temperature", temperatureRepository.findAll());
+        model.addAttribute("xValues", xValues);
+        model.addAttribute("yValues", yValues);
+
         return "temperatureView";
     }
 }
